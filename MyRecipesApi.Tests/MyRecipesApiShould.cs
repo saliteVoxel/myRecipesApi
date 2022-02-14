7284d0f2bb2d360edf8a2fe8f0631ba2757f90cb
salite <salite@voxelgroup.net>
2022-02-14 16:32:18 +0100

    chore: create project structure

---
+++ MyRecipesApi.Tests/MyRecipesApiShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace MyRecipesApi.Tests
+{
+    public class MyRecipesApiShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
