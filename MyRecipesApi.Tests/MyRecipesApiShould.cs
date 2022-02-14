9f329a584ad36e8b7464c75175b56964b7227905
salite <salite@voxelgroup.net>
2022-02-14 17:00:26 +0100

    test: add recipe

--- MyRecipesApi.Tests/MyRecipesApiShould.cs
+++ MyRecipesApi.Tests/MyRecipesApiShould.cs
@@ -6,8 +6,15 @@ namespace MyRecipesApi.Tests
     public class MyRecipesApiShould
     {
         [Fact]
-        public void Test1()
+        public void AddRecipe()
         {
+            //Recipes es el nombre de la clase, y recipes es la instancia del objeto
+            //Arrange
+            var recipes = new Recipes();
+            //Act -> recipes.Add()
+            recipes.Add("Pimientos con patatas");
+            //Assert
+            Assert.Equal("Pimientos con patatas",recipes.CheckRecipe("Pimientos con patatas"));
         }
     }
 }
---
+++ MyRecipesApi/Recipes.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace MyRecipesApi
+{
+    public class Recipes
+    {
+        public void Add(string recipe)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<char> CheckRecipe(string recipe)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
